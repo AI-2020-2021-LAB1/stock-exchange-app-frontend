c54e71f297d412a8b9f25e3cb8024c2cafcee2cf
Kamil Klecha <urlikodd@gmail.com>
2020-10-18 19:47:11 +0200

    Added some dummy text on Index to test auto deployment

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        <p>Test</p>
-    </div>
+  <div>
+    <p class="text-center ma-5">
+      Strona główna aplikacji do handlowania akcjami, w ramach projektu z
+      przedmiotu Aplikacje Internetowe.<br />Opiekun: dr. inż. Tomasz Rak
+    </p>
+  </div>
 </template>
