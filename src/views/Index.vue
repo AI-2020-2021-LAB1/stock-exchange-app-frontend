117ce1242177d8bcc83d58f2ad313659755dd94c
Wojciech Gołda <w.golda@praktyki.ideo.pl>
2020-10-19 15:28:06 +0200

    change .js to .ts

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,7 +2,7 @@
   <div>
     <p class="text-center ma-5">
       Strona główna aplikacji do handlowania akcjami, w ramach projektu z
-      przedmiotu Aplikacje Internetowe.<br />Opiekun: dr. inż. Tomasz Rak
+      przedmiotu Aplikacje Internetowe<br />Opiekun: dr. inż. Tomasz Rak
     </p>
   </div>
 </template>
