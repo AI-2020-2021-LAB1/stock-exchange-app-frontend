536f501207a15d147355701781f13797deddb143
sterownik <4golda4@gmail.com>
2020-11-12 19:30:57 +0100

    center element in User Tab

---
+++ src/views/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row class="fill-height" align="center">
+    <v-card-actions class="mr-auto ml-auto" justify="center">
+      <UserInformation />
+    </v-card-actions>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import UserInformation from '../components/UserInformation.vue';
+
+@Component({
+  components: {
+    UserInformation,
+  },
+})
+export default class User extends Vue {}
+</script>
