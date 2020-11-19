23de8b72e4ece692172927db9475cba5f65c7b65
Patryk Marchut <pm9701133@gmail.com>
2020-11-19 21:07:54 +0100

    dodanie textBoxow i checkboxa do formularza

--- src/views/Benchmark.vue
+++ src/views/Benchmark.vue
@@ -8,34 +8,45 @@
               >Konfiguracja</v-toolbar-title
             >
           </v-toolbar>
-          <v-form v-model="inputValidated" @submit.prevent="registerUser()">
+          <v-form>
             <v-card-text class="pb-0">
-              <v-text-field></v-text-field>
               <v-row align="center" justify="center" class="mx-0">
                 <v-col class="pa-0">
-                  <v-text-field></v-text-field>
+                  <v-text-field
+                    outlined
+                    hide-details
+                    v-model.number="opCount"
+                    label="liczba operacji"
+                    color="primary"
+                    type="number"
+                  ></v-text-field>
                 </v-col>
                 <v-col class="py-0 pr-0">
-                  <v-text-field></v-text-field>
+                  <v-text-field
+                    outlined
+                    hide-details
+                    v-model.number="opPrice"
+                    label="kwota pojedynczej operacji"
+                    color="primary"
+                    type="number"
+                  ></v-text-field>
                 </v-col>
               </v-row>
-              <v-row align="center" justify="center" class="mx-0">
-                <v-col class="pa-0">
-                  <v-text-field></v-text-field>
-                </v-col>
-                <v-col class="py-0 pr-0">
-                  <v-text-field></v-text-field>
+              <v-row>
+                <v-col>
+                  <v-checkbox
+                    color="primary"
+                    v-model="checkboxUser"
+                    label="czy rejestrować nowego użytkownika?"
+                  ></v-checkbox>
                 </v-col>
               </v-row>
-              <div v-if="passwdFocus">
-                <password-validator></password-validator>
-              </div>
             </v-card-text>
             <v-card-actions class="pt-0">
               <v-spacer></v-spacer>
-              <v-btn :disabled="!inputValidated" color="primary" type="submit">
-                <span class="font-weight-bold">Zarejestruj się</span>
-                <v-icon right>mdi-database-plus</v-icon>
+              <v-btn color="primary" type="submit">
+                <span class="font-weight-bold">rozpocznij test</span>
+                <v-icon right>mdi-database-check</v-icon>
               </v-btn>
             </v-card-actions>
           </v-form>
@@ -44,3 +55,18 @@
     </v-col>
   </v-row>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Register extends Vue {
+  private data() {
+    return {
+      opCount: 1,
+      opPrice: 100,
+      checkboxUser: false,
+    };
+  }
+}
+</script>
