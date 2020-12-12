126cf1287eb16653e3f1617ee53f6cb7ad308e09
Patryk Marchut <pm9701133@gmail.com>
2020-12-12 20:36:44 +0100

    Paginacja i filtrowanie ofert i transakcji związanych z akcją by abbreviation/anulacja aktywnych ofert

--- src/API/transactions.ts
+++ src/API/transactions.ts
@@ -4,8 +4,19 @@ import { Transactions } from '../models/TransactionModel';
 import store from '../store';
 
 export class TransactionsService {
-    public async getTransactions(body: object): Promise<AxiosResponse<Transactions>> {
+    public async getUserTransactions(body: object): Promise<AxiosResponse<Transactions>> {
         const transactions = await axios.get('api/user/transaction/owned', {
+            headers: {
+                Authorization: 'Bearer ' + store.getters.token,
+            },
+            params: body,
+        });
+
+        return transactions;
+    }
+
+    public async getTransactions(body: object): Promise<AxiosResponse<Transactions>> {
+        const transactions = await axios.get('api/transaction', {
             headers: {
                 Authorization: 'Bearer ' + store.getters.token,
             },
