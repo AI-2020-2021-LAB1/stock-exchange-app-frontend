9d4fa486240946c59d3bc21b27ed5baf6979445f
Patryk Marchut <pm9701133@gmail.com>
2020-12-14 13:24:29 +0100

    hotfix formatowania daty do tabelek

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,16 +1,17 @@
-export function formatDate(date: string) {
-    return `${date.substring(
-        0,
-        10,
-    )} ${date.substring(11, 19)}`;
+export function formatDate(date: string): string {
+    return new Date(date).toLocaleString();
+    // return `${date.substring(
+    //     0,
+    //     10,
+    // )} ${date.substring(11, 19)}`;
 }
 
-export function formatTime() {
+export function formatTime(): string {
     const time = new Date();
     return `${time.getHours()}:${time.getMinutes()}`;
 }
 
-export function formatDateBenchmark() {
+export function formatDateBenchmark(): string {
     const time = new Date();
     return `${time.getUTCFullYear()}-${(time.getMonth() + 1) < 10 ? '0' + (time.getMonth() + 1) : (time.getMonth() + 1)}-${time.getDate() < 10 ? '0' + time.getDate() : time.getDate()}`;
 }
