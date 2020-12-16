a720ab59053745068779e9ecc945070f47b3223e
Kamil Klecha <urlikodd@gmail.com>
2020-12-16 23:14:57 +0100

    Fixed axios instantiation

--- src/axiosBenchmark.ts
+++ src/axiosBenchmark.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = process.env.VUE_APP_BACK_URL || 'http://127.0.0.1:8081/';
-axios.defaults.baseURL = 'http://193.33.111.196:9000/';
+const benchmarkAxios = axios.create({
+    baseURL: process.env.VUE_APP_BENCHMARK_URL,
+});
 
-export default axios;
+export default benchmarkAxios;
