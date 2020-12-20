6e760cd6d524c6579cc9a838f9a50c61f75095be
Kamil Klecha <urlikodd@gmail.com>
2020-12-20 17:39:30 +0100

    Removed unneeded code

--- src/API/tests.ts
+++ src/API/tests.ts
@@ -1,14 +1,9 @@
 import axios from '../axiosBenchmark';
 import { AxiosResponse } from 'axios';
-import store from '../store';
 
 export class TestsService {
     public async startTest(body: object): Promise<AxiosResponse> {
-        const test = await axios.post('api/test/', body, {
-            headers: {
-                Authorization: 'Bearer ' + store.getters.token,
-            },
-        });
+        const test = await axios.post('api/test/', body);
         return test;
     }
 
